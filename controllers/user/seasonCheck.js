953b2a57a153ec6785acbc69c68d143f393c5133
Elcoputra <elcoputra94@gmail.com>
2020-06-25 16:36:09 +0700

    handle waktu subscribe ketika 0

--- controllers/user/seasonCheck.js
+++ controllers/user/seasonCheck.js
@@ -1,13 +1,47 @@
 const { user } = require('../../models');
+const { transaction } = require('../../models');
+const Moment = require('moment');
+const { subscribe } = require('../../api');
 
 /* GET users listing. */
 exports.reads = async (req, res) => {
   try {
+    // Logic subscribe false bila usah di di bawa 0 hari //
+    // cari data transaksi dari user
+    const trans = await transaction.findOne({
+      where: {
+        userId: req.decode.id,
+      },
+    });
+
+    // menghitung jumlah hari
+    if (trans) {
+      const subscribeDays = Moment(trans.dueDate).diff(Date.now(), 'days');
+      console.log(subscribeDays);
+      if (subscribeDays <= 0) {
+        await user.update(
+          { subscribe: false },
+          {
+            where: {
+              id: req.decode.id,
+            },
+          },
+        );
+        await transaction.destroy({
+          where: {
+            userId: req.decode.id,
+          },
+        });
+      }
+    }
+
+    // cari user sesuai id dari token yang sudah di decode
     const userdata = await user.findOne({
       where: {
         id: req.decode.id,
       },
     });
+
     return res.send({
       data: {
         id: userdata.id,
