277ab8cdc9f8c34d183bd89003dd774a76a8a01b
Elcoputra <elcoputra94@gmail.com>
2020-06-26 11:53:01 +0700

    useless fix

--- api/middlwares/token.js
+++ api/middlwares/token.js
@@ -4,15 +4,11 @@ const jwt = require('jsonwebtoken');
 
 exports.getToken = async (req, res) => {
   try {
-    // logic untuk nanti
-    // kalo role admin dapet token admin, kalo user dapet token regular
-    // atau
-    // kalo admin dapet token, kalo user tidak dapat token
-
     const { email } = req.body;
     const idUser = req.credentialUser.id;
     const isAdmin = req.credentialUser.isAdmin;
-    const token = jwt.sign({ id: idUser, isAdmin: isAdmin }, process.env.TOKEN_KEY);
+    const isLogin = req.credentialUser.isLogin;
+    const token = jwt.sign({ id: idUser, isAdmin: isAdmin, isLogin: isLogin }, process.env.TOKEN_KEY);
     return res.send({
       message: 'Login Success',
       data: [{ id: idUser, email, isAdmin, token }],
