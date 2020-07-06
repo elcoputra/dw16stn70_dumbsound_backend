b3c90eaeb0fa928cd04d2398a6b15d357c0e5043
Elcoputra <elcoputra94@gmail.com>
2020-07-06 20:11:21 +0700

    test

--- models/index.js
+++ models/index.js
@@ -11,6 +11,7 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
+  console.log(process.env.DATABASE_URL);
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
