b7bf0a4d96c099f63983403a1f7032917bbb881e
Elcoputra <elcoputra94@gmail.com>
2020-07-06 20:30:26 +0700

    🤷‍♂️ test

--- models/index.js
+++ models/index.js
@@ -6,12 +6,11 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  console.log(process.env.DATABASE_URL);
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
